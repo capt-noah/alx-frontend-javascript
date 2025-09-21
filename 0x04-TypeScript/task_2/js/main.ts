e9cc0345c451da5ca27b21da055f321c90e66928
Alem-Mamo <119974698+Alem-Mamo@users.noreply.github.com>
2025-09-21 16:08:50 +0300

    task-1/12

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -44,3 +44,16 @@ export function createEmployee(salary: number): Director | Teacher {
   }
   return new Director();
 }
+
+// import { Director, Teacher, DirectorInterface, TeacherInterface } from "./main";
+
+export function isDirector(employee: DirectorInterface | TeacherInterface): employee is Director {
+  return (employee as Director).workDirectorTasks !== undefined;
+}
+
+export function executeWork(employee: DirectorInterface | TeacherInterface): string {
+  if (isDirector(employee)) {
+    return employee.workDirectorTasks();
+  }
+  return (employee as Teacher).workTeacherTasks();
+}
